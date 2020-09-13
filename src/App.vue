a157f42a4b4f497b76b922e2ff47f46ceed2ac57
evanzummeren <erikvanzummeren@gmail.com>
2020-09-12 21:53:40 -0400

    sticky

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,113 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="hero">
+      <h1>Understanding radical<br>YouTube communities</h1>
+      <span class="cta">Why and what?</span>
+      <div class="line"></div>
+      <video playsinline autoplay muted poster="polina.jpg">
+        <source src="./assets/videobg.mp4" type="video/mp4">
+      </video>
+    </div>
+    <Introduction />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Introduction from "./components/Introduction.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Introduction
   }
 };
 </script>
 
 <style>
+* {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-size: 16px;
+}
+
+@font-face {
+  font-family: "GilReg";
+  src: url("./assets/fonts/gil-reg.woff2") format("woff2"),
+       url("./assets/fonts/gil-reg.woff") format("woff");
+}
+
+@font-face {
+  font-family: "Flaco";
+  src: url("./assets/fonts/fl-mono.woff") format("woff");
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  background: #000000;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* display: flex; */
+  flex-direction: row;
+  flex-wrap: nowrap;
+}
+
+.hero {
+  min-width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+
+h1 {
+  color: white;
+  position: absolute;
+  font-family: "GilReg";
+  z-index: 100;
+  /* margin-top: 33vh; */
+  font-size: 4rem;
+  text-align: center;
+}
+
+.cta {
+  color: white;
+  position: absolute;
+  z-index: 100;
+  margin-top: 12rem;
+  font-family: "Flaco";
+}
+
+@keyframes moveBg {
+  from {background-position: 0 0;}
+  to {background-position: 0 425px;}
+}
+
+.line {
+  position: absolute;
+  z-index: 99;
+  margin-top: 42rem;
+  background-image: url("./assets/line.svg");
+  background-size: cover;
+  width: 11px;
+  height: 425px;
+  animation-name: moveBg;
+  animation-duration: 5s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+}
+
+video {
+  object-fit: cover;
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
---
+++ src/components/Introduction.vue
@@ -0,0 +1,200 @@
+<template>
+  <div class="introduction">
+    <svg>
+      <g v-for="vindex in 90" :key="vindex">
+      <g v-for="index in widthPoints" :key="index">
+        <line class="st0" :x1="64 * index" :y1="vindex * 64 - 64" :x2="64 * index" :y2="vindex * 64 + 6 - 64"/>
+        <line class="st0" :x1="64 * index + 3" :y1="vindex * 64 - 64 + 3" :x2="64 * index - 3" :y2="vindex * 64 - 64 + 3"/>
+      </g>
+      </g>
+    </svg>
+
+    <div class="bg__elem__one"></div>
+    <div class="bg__elem__two"></div>
+    <div class="bg__elem__three"></div>
+    <div class="bg__elem__four"></div>
+
+    <div class="step step__one"></div>
+    <div class="step step__two"></div>
+    <div class="step step__three"></div>
+    <div class="step step__four"></div>
+    <div class="step step__five"></div>
+
+    <h2>{{text}}</h2>
+
+  </div>
+</template>
+
+<script>
+import scrollama from "scrollama";
+
+export default {
+  name: "Introduction",
+  data: function() {
+    return {
+      text: "Every minute more than 500 hours of video is uploaded to YouTube",
+      widthPoints: 10,
+    }
+  },
+  created: function() {
+    this.widthPoints = Math.ceil(window.innerWidth/64);
+  },
+  mounted: function() {
+    const scroller = scrollama();
+
+    // setup the instance, pass callback functions
+      scroller
+        .setup({
+          step: ".step"
+        })
+        .onStepEnter(response => {
+          console.log(response)
+          if (response.index === 0) {
+            this.text = "Every minute more than 500 hours of video is uploaded to YouTube"
+          } else if (response.index === 1) {
+            this.text = "Some of these videos have a devastating effect on our society"            
+          } else if (response.index === 2) {
+            this.text = "They polarize groups to each other"            
+          } else if (response.index === 3) {
+            this.text = "And radicalize individuals"            
+          } else if (response.index === 4) {
+            this.text = "However, due to YouTube's algorithmic nature, it's often difficult to understand what's happening in the dark corners of the platform"            
+          }
+
+//           However,  due to YouTube’s algorithmic nature, it’s often difficult
+// to understand what’s happening in the dark corners of the platform
+          // { element, index, direction }
+        })
+        // .onStepExit(response => {
+        //   // console.log(response)
+        //   // { element, index, direction }
+        // });
+
+      // setup resize event
+      window.addEventListener("resize", scroller.resize);
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.introduction {
+  background: black;
+  min-height: 400vh;
+  min-width: 100%;
+  display: flex;
+}
+
+h2 {
+  position: -webkit-sticky; /* for Safari */
+  position: sticky;
+  color: white;
+  width: 100vw;
+  padding-left: 20%;
+  padding-right: 20%;
+  text-align: center;
+  font-family: "GilReg";
+  font-size: 2rem;
+  margin-top: 20rem;
+  align-self: flex-start;
+  top: 50%;
+}
+
+p {
+  color: black;
+}
+
+svg {
+  width: 100%;
+  height: 400vh;
+  background: black;
+  position: absolute;
+}
+
+.st0{
+  fill:none;
+  stroke:#575756;
+  stroke-width:0.5;
+  stroke-miterlimit:10;
+}
+
+.bg__elem__one {
+  border-radius: 100%;
+  width: 3rem;
+  height: 3rem;
+  background-image: url("../assets/crosshair.svg"), url("../assets/noise_gif1.gif");
+  background-position: center center;
+  background-size: 6px, cover;
+  background-repeat: no-repeat;
+  position: absolute;
+  margin-left: 14.5rem;
+  margin-top: 6.71rem;
+}
+
+.bg__elem__two {
+  border-radius: 100%;
+  width: 3rem;
+  height: 3rem;
+  background-image: url("../assets/crosshair.svg"), url("../assets/noise_gif2.gif");
+  background-position: center center;
+  background-size: 6px, cover;
+  background-repeat: no-repeat;
+  position: absolute;
+  margin-left: 22.5rem;
+  margin-top: 50.71rem;
+}
+
+.bg__elem__three {
+  clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+  width: 3rem;
+  height: 3rem;
+  background-image: url("../assets/crosshair.svg"), url("../assets/noise_gif3.gif");
+  background-position: center center;
+  background-size: 6px, cover;
+  background-repeat: no-repeat;
+  position: absolute;
+  margin-left: 70.5rem;
+  margin-top: 14.71rem;
+}
+
+.bg__elem__four {
+  clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+  width: 3rem;
+  height: 3rem;
+  background-image: url("../assets/crosshair.svg"), url("../assets/noise_gif4.gif");
+  background-position: center center;
+  background-size: 6px, cover;
+  background-repeat: no-repeat;
+  position: absolute;
+  margin-left: 90.5rem;
+  margin-top: 30.71rem;
+}
+
+.step {
+  width: 10px;
+  height: 10px;
+  /* background: white; */
+  position: absolute;
+}
+
+.step__one {
+  margin-top: 50vh;
+}
+
+.step__two {
+  margin-top: 100vh;
+}
+
+.step__three {
+  margin-top: 150vh;
+}
+
+.step__four {
+  margin-top: 200vh;
+}
+
+.step__five {
+  margin-top: 250vh;
+}
+
+</style>
